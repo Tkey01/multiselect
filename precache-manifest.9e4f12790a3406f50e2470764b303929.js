7114afac707f4ae1d40e462550db486f3f1e6689
tkey01 <forwebmoney5@yandex.ru>
2020-03-29 23:39:10 +0300

    Updates

---
+++ precache-manifest.9e4f12790a3406f50e2470764b303929.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b1aa53568a6ef3faa7679e57d3793f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7db70c5062b4df009a99",
+    "url": "/static/css/main.5b1d78a4.chunk.css"
+  },
+  {
+    "revision": "f7ae87ddeb5330925cbf",
+    "url": "/static/js/2.0aae2654.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.0aae2654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7db70c5062b4df009a99",
+    "url": "/static/js/main.82e9bc1c.chunk.js"
+  },
+  {
+    "revision": "22c72b9c9005900de6cb",
+    "url": "/static/js/runtime-main.6f3b0c78.js"
+  }
+]);
